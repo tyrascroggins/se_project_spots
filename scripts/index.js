df0935d983f65247a4fc468a6bd79ff57345c366
Tyra <tyrascroggins@gmail.com>
2024-09-05 16:12:35 -0400

    p4#1

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const initialCards = [
+    { Name: "Val Thorens", Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg" },
+    { Name: "Restaurant terrace", Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg" },
+    { Name: "An outdoor cafe", Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg" },
+    { Name: "A very long bridge, over the forest and through the trees", Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg" },
+    { Name: "Tunnel with morning light", Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg" },
+    { Name: "Mountain house", Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg" }
+  ];
+  
+console.log(initialCards);
